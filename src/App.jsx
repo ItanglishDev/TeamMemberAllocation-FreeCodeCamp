b42f723391ff0cca395687eaa3c510788357a06a
Chris Hetherton <>
2022-10-11 15:57:16 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import * as React from 'react'
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+
+    </div>
+  )
+}
