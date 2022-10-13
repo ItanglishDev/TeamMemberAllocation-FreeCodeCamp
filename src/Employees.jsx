d8b721fb378c6f3cb0e1b031670ac9821a89a7e6
Chris Hetherton <78489707+ItanglishDev@users.noreply.github.com>
2022-10-13 15:13:49 +0000

    added ternary operator for gender

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -94,12 +94,29 @@ const Employees = () => {
 
     <main className='container' >
       <div class='row justify-content-center mt-3 mb-3 ' >
+        <div class='row justify-content-center mt-3 mb-3 ' >
+          <div className='col-8' >
+            <select>
+              <option value='TeamA' >TeamA</option>
+              <option value='TeamB' >TeamB</option>
+              <option value='TeamC' >TeamC</option>
+              <option value='TeamD' >TeamD</option>
+
+            </select>
+
+          </div>
+        </div>
+
         <div className='col-8' >
           <div className='card-collection' >
             {
               employees.map((employee) => (
                 <div id={employee.id} className='card m-2' style={{ cursor: 'pointer' }} >
-                  <img src={femaleProfile} className='card-img-top' />
+                  {(employee.gender === 'male') ?
+
+                    <img src={maleProfile} className='card-img-top' /> :
+                    <img src={femaleProfile} className='card-img-top' />}
+
                   <div className='card-body' >
                     <h5 className='card-title' >Full Name: {employee.fullName} </h5>
                     <p className='card-text' >Designation: {employee.designation}
